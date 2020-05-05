3ff32f06b91ea945705a8824ebfe7dd21950d0c4
adash <abhishekdash.work@gmail.com>
2020-05-06 01:30:16 +0530

    jumbotron style update

--- src/UI/ui/Home/JumbotronPannel/JumbotronPannel.js
+++ src/UI/ui/Home/JumbotronPannel/JumbotronPannel.js
@@ -1,29 +1,50 @@
 import styles from 'styled-components'
 import React from 'react';
+import { HighLighter} from '../CarouselPannel/CarouselCards';
 
 const JPannel = styles.div`
-
 display: flex;
-flex-direction: column;
 background: #343a90;
 margin-top: 20px;
-padding: 50px;
 color: white;
 border-radius: 5px;
 box-shadow: 0 10px 6px -6px #777;
 margin:30px 0px 10px 0px;
+`
+const BlogTextSection = styles.div`
+
+display:flex;
+flex-direction:column;
+
+padding :50px;
 
 `
+
+const ImageSection = styles.div`
+display:flex;
+background: white;
+flex-basis:70%
+bacbackground-size: cover;
+`
+
+
 const JumbotronPannel = ({blog}) => {
     return (
         <JPannel>
+              <BlogTextSection>
               <h1>
                 {blog.title}
                 </h1>
+                <HighLighter/>
+                <br/>
                 <h3>{blog.subTitle}</h3> 
-                <br />
-                <h5>{blog.author}</h5>   
-
+                
+                {/* <br />
+                <h5>{blog.author}</h5>    */}
+            </BlogTextSection>
+            <ImageSection>
+                <img className = "rounded img-fluid" src ="https://storage.googleapis.com/chydlx/codepen/blog-cards/image-2.jpg"></img>
+            </ImageSection>
         </JPannel>
     )
 
